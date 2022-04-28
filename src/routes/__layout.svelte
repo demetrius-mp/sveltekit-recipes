48b7f4ad90f8fbeafc2ef3709613a42f27a1691c
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-04-27 21:16:43 -0400

    refactor: remove unused functions

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { session } from '$app/stores';
 	import { Navbar } from '$lib/components/layout';
 	import { ToastContainer, toastStore } from '$lib/components/Toast';
-	import userStore from '$lib/stores/user.store';
 	import 'bootstrap-icons/font/bootstrap-icons.css';
 	import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,21 +9,6 @@
 		$session.flashMessages.forEach((toast) => {
 			toastStore.addToast(toast);
 		});
-	}
-
-	async function gotoSignIn() {
-		await goto('/sign-in');
-	}
-
-	async function signOut() {
-		await userStore.signOut();
-		toastStore.addToast({
-			title: 'Success',
-			body: 'Signed out successfully!',
-			color: 'success',
-			removeAfter: 1000
-		});
-		await goto('/');
 	}
 </script>
 
