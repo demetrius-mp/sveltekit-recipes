711f56b48cc657fb4b5bf8a485a2c6958fe476e6
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-04-29 18:18:56 -0400

    feat: async pagination with loading animation

---
+++ src/lib/stores/post.store.ts
@@ -0,0 +1,134 @@
+import type { Post } from '$lib/types';
+import { writable, type Writable } from 'svelte/store';
+
+type LoadReturnType = {
+	posts: Post[];
+	pageSize: number;
+	totalItems: number;
+};
+
+type PostStore = {
+	subscribe: Writable<Post[]>['subscribe'];
+	set: Writable<Post[]>['set'];
+	update: Writable<Post[]>['update'];
+
+	load(info: { pageNumber: number }): Promise<LoadReturnType>;
+};
+
+function createPostStore(): PostStore {
+	const { set, subscribe, update } = writable<Post[]>([]);
+
+	return {
+		subscribe,
+		set,
+		update,
+		async load({ pageNumber }) {
+			const { posts, totalItems, pageSize } = await loadPagedPosts(pageNumber);
+			set(posts);
+			return { posts, totalItems, pageSize };
+		}
+	};
+}
+
+const postStore = createPostStore();
+
+export default postStore;
+
+// delete this
+
+// create a stall function
+async function stall(ms: number): Promise<void> {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// simulate fetch api
+async function loadPagedPosts(pageNumber: number) {
+	await stall(1000);
+	const POSTS_PER_PAGE = 2;
+
+	const start = (pageNumber - 1) * POSTS_PER_PAGE;
+	const end = start + POSTS_PER_PAGE;
+	const pagedPosts = posts.slice(start, end);
+
+	return {
+		posts: pagedPosts,
+		totalItems: posts.length,
+		pageSize: POSTS_PER_PAGE
+	};
+}
+
+// fake db
+const posts: Post[] = [
+	{
+		id: 1,
+		title: 'Hello World',
+		description: 'This is a sample post',
+		tags: ['hello', 'world'],
+		body: 'Hello World',
+		createdAt: new Date()
+	},
+	{
+		id: 2,
+		title: 'Hello World 2',
+		description: 'This is a sample post 2',
+		tags: ['hello', 'world'],
+		body: 'Hello World 2',
+		createdAt: new Date()
+	},
+	{
+		id: 3,
+		title: 'Hello World 3',
+		description: 'This is a sample post 3',
+		tags: ['hello', 'world'],
+		body: 'Hello World 3',
+		createdAt: new Date()
+	},
+	{
+		id: 4,
+		title: 'Hello World 4',
+		description: 'This is a sample post 4',
+		tags: ['hello', 'world'],
+		body: 'Hello World 4',
+		createdAt: new Date()
+	},
+	{
+		id: 5,
+		title: 'Hello World 5',
+		description: 'This is a sample post 5',
+		tags: ['hello', 'world'],
+		body: 'Hello World 5',
+		createdAt: new Date()
+	},
+	{
+		id: 6,
+		title: 'Hello World 6',
+		description: 'This is a sample post 6',
+		tags: ['hello', 'world'],
+		body: 'Hello World 6',
+		createdAt: new Date()
+	},
+	{
+		id: 7,
+		title: 'Hello World 7',
+		description: 'This is a sample post 7',
+		tags: ['hello', 'world'],
+		body: 'Hello World 7',
+		createdAt: new Date()
+	},
+	{
+		id: 8,
+		title: 'Hello World 8',
+		description: 'This is a sample post 8',
+		tags: ['hello', 'world'],
+		body: 'Hello World 8',
+		createdAt: new Date()
+	},
+	{
+		id: 9,
+		title: 'Hello World 9',
+		description: 'This is a sample post 9',
+		tags: ['hello', 'world'],
+		body: 'Hello World 9',
+		createdAt: new Date()
+	}
+];
--- src/routes/app/posts/index.svelte
+++ src/routes/app/posts/index.svelte
@@ -1,14 +1,14 @@
 <script lang="ts" context="module">
 	import Paginator from '$lib/components/Paginator';
 	import type { HandlePageChange } from '$lib/components/Paginator/Paginator.svelte';
+	import postStore from '$lib/stores/post.store';
 	import type { Post } from '$lib/types';
 	import { formatDate } from '$lib/utils/formatter.utils';
 	import type { Load } from '@sveltejs/kit';
-	import { getPosts } from './_api';
 
 	export const load: Load = async () => {
-		const { posts, pageSize, totalItems } = await getPosts({
-			page: 1
+		const { posts, pageSize, totalItems } = await postStore.load({
+			pageNumber: 1
 		});
 
 		return {
@@ -27,40 +27,74 @@
 	export let totalItems: number;
 
 	let currentPage: number = 1;
+	let isFirstLoad: boolean = true;
 	const handlePageChange: HandlePageChange = async (e) => {
 		currentPage = e.detail;
-		const { posts: pagedPosts } = await getPosts({
-			page: currentPage
+		isFirstLoad = false;
+	};
+
+	let isLoading: boolean = false;
+	async function loadPosts(pageNumber: number) {
+		isLoading = true;
+		const { posts: pagedPosts } = await postStore.load({
+			pageNumber
 		});
 
 		posts = pagedPosts;
-	};
+		isLoading = false;
+	}
+
+	$: {
+		if (!isFirstLoad) {
+			loadPosts(currentPage);
+		}
+	}
 </script>
 
 <div class="container mt-3">
-	<h1 class="lead fs-1 text-center">Posts</h1>
+	<div class="d-flex justify-content-between">
+		<div>
+			<h1 class="lead fs-1 text-center">Posts</h1>
+		</div>
+		<div>
+			<Paginator {totalItems} {currentPage} {pageSize} on:pagechange={handlePageChange} />
+		</div>
+	</div>
 	<div class="row row-cols-1 g-3 row-cols-md-2">
-		{#each posts as post}
-			<div class="col">
-				<div class="card shadow-sm">
-					<div class="card-header d-flex justify-content-between">
-						<div>
-							{post.title}
-						</div>
-						<div>
-							<span class="text-muted small">
-								{formatDate(post.createdAt)}
-							</span>
-						</div>
-					</div>
-					<div class="card-body">
-						<p class="m-0">{post.description}</p>
-					</div>
+		{#if isLoading}
+			<div class="text-center" style="margin-top: 100px;">
+				<div class="spinner-border spinner-size" role="status">
+					<span class="visually-hidden">Loading...</span>
 				</div>
 			</div>
-		{/each}
+		{:else}
+			{#each posts as post}
+				<div class="col">
+					<div class="card shadow-sm">
+						<div class="card-header d-flex justify-content-between">
+							<div>
+								{post.title}
+							</div>
+							<div>
+								<span class="text-muted small">
+									{formatDate(post.createdAt)}
+								</span>
+							</div>
+						</div>
+						<div class="card-body">
+							<p class="m-0">{post.description}</p>
+						</div>
+					</div>
+				</div>
+			{/each}
+		{/if}
 	</div>
-	<div class="mt-3 d-flex justify-content-end">
-		<Paginator {totalItems} {currentPage} {pageSize} on:pagechange={handlePageChange} />
-	</div>
+	<div class="mt-3 d-flex justify-content-end" />
 </div>
+
+<style>
+	.spinner-size {
+		width: 3rem;
+		height: 3rem;
+	}
+</style>
