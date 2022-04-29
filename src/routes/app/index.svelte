69be8ce2d9fa8cbec06f5cfc2cfdda4d13f33bb2
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-04-29 11:01:24 -0400

    feat: app index page

--- src/routes/app/index.svelte
+++ src/routes/app/index.svelte
@@ -1,5 +1,41 @@
 <script lang="ts">
 	import { session } from '$app/stores';
+
+	type Option = {
+		path: string;
+		name: string;
+		icon: string;
+	};
+
+	const options: Option[] = [
+		{
+			name: 'Manage posts',
+			icon: 'postcard',
+			path: '/app/posts'
+		}
+	];
 </script>
 
-<h1>Welcome to the app, {$session.user?.name}</h1>
+<div class="container mt-3">
+	<h1 class="text-center lead fs-1">Welcome back, {$session.user?.name}</h1>
+
+	<div class="mt-3">
+		<div class="row row-cols-1 g-3">
+			{#each options as option}
+				<div class="col">
+					<div
+						class="card shadow p-3 d-flex flex-column justify-content-center align-items-center"
+						style="border-radius: 0.5rem;"
+					>
+						<i class="bi bi-{option.icon}" style="font-size: 5rem;" />
+						<h3 class="m-0">
+							<a class="text-decoration-none text-black stretched-link" href={option.path}>
+								{option.name}
+							</a>
+						</h3>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
